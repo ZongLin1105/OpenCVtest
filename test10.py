0dd5088ea8cf67ddd954c88382bef51a0064d877
ZongLin1105 <james83511ab@yahoo.com.tw>
2021-01-29 10:01:15 +0800

    練習到物體追蹤

--- test10.py
+++ test10.py
@@ -4,13 +4,16 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread("123.PNG", 1)  #0為黑白,1為彩色
 b = cv2.imread("123.PNG", 1)
+e1 = cv2.getTickCount() #計算執行時間
 img1 = b[200:400, 200:400]
 img2 = b[300:500, 400:600]
-print(img1)
-print(img2)
+# print(img1)
+# print(img2)
 dst=cv2.addWeighted(img1, 0.7, img2, 0.3, 0) 
 b[300:500, 400:600] = dst
-
+e2 = cv2.getTickCount() #計算執行時間
+t = (e2-e1) / cv2.getTickFrequency() #計算出執行處理影像時間
+print(t)
 cv2.namedWindow("123", cv2.WINDOW_NORMAL) #調整影像大小
 cv2.imshow("123", img) #顯示影像
 cv2.imshow("b", b)
---
+++ test11.py
@@ -0,0 +1,5 @@
+import cv2
+flags=[i for i in dir(cv2) if i.startswith('COLOR_')]
+print (lens(flags))
+
+# cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
