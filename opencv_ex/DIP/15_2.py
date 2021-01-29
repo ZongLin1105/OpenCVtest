737591219c6ccf4b7a30ef5313ae73f42a1c587d
ZongLin1105 <james83511ab@yahoo.com.tw>
2021-01-29 14:33:19 +0800

    閥值練習

--- opencv_ex/DIP/15_2.py
+++ opencv_ex/DIP/15_2.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('test13.jpg', 0)
+img = cv2.imread('../image/test13.jpg', 0)
 img = cv2.medianBlur(img, 5)  # 中值滤波
 
 ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 # 11 为 Block size, 2 为 C 值
-th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2) #取平均,+-2當閥值
+th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5) #取高斯,+-5當閥值
 titles = ['Original Image',
           'Global Thresholding (v = 127)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
 images = [img, th1, th2, th3]
--- opencv_ex/DIP/15_3.py
+++ opencv_ex/DIP/15_3.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('test14.jpg', 0)
+img = cv2.imread('../image/test14.jpg', 0)
 # global thresholding
-ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) #>127白 <127黑
 # Otsu's thresholding
 ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 # Otsu's thresholding after Gaussian filtering
